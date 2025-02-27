2c847112a21ae1bab00d94747032418a0d056ab8
Ish <28477177+byte-ish@users.noreply.github.com>
2025-02-27 06:15:38 +0000

    Add files via upload

---
+++ springjourney/src/test/java/com/bits8byte/springjourney/SpringjourneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bits8byte.springjourney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringjourneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
